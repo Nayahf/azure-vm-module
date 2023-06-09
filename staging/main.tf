56c427757fb0e2968273e3ba8c9edd92c0cace94
hashi-netbiz <rachelngunjoh@gmail.com>
2023-06-08 23:37:45 -0400

    cloudinit and ssh key added

--- staging/main.tf
+++ staging/main.tf
@@ -11,6 +11,17 @@ data "azurerm_subnet" "vm_subnet" {
   resource_group_name  = var.resource_group_name
 }
 
+# Data template cloud-init bootstrapping file
+data "local_file" "cloudinit" {
+  filename = "${path.module}/cloudinit.conf"
+}
+
+# Create (and display) an SSH key
+resource "tls_private_key" "ssh_key" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+} 
+
 /* 
 data "vault_generic_secret" "admin_name" {
   path = "<secret_path>"
@@ -18,6 +29,12 @@ data "vault_generic_secret" "admin_name" {
 
 data "vault_generic_secret" "admin_password" {
   path = "<secret_path>"
+}
+
+OR
+
+data "vault_generic_secret" "my_secret" {
+  path = "secret/path/to/your/secret"
 }
 
 */
@@ -40,7 +57,23 @@ module "virtual-machine" {
     os_disk_type            = var.os_disk_type
   # admin_username        = data.vault_generic_secret.admin_name.data.username
   # admin_password        = data.vault_generic_secret.admin_password.data.password
+  # OR
+  # admin_username        = data.vault_generic_secret.my_secret.data["my_USER_key"]
+  # admin_password        = data.vault_generic_secret.my_secret.data["my_PASS_key"]
+
     admin_username          = var.admin_username
     admin_password          = var.admin_password
     image_id                = data.azurerm_shared_image_version.img.id
+    customdata_cloudinit    = data.local_file.cloudinit.content
+    ssh_public_key = tls_private_key.ssh_key.public_key_openssh
 }
+
+# # Push private key back up to vault
+# resource "vault_generic_secret" "my_secret" {
+#   path = "secret/data/${var.vmname}-vm-ssh-key"
+
+#   data = {
+#     vm_name = var.name
+#     ssh_private_key = tls_private_key.ssh_key.id
+#   }
+# }
--- staging/output.tf
+++ staging/output.tf
@@ -12,3 +12,17 @@ output "new_vm_id" {
     description = "id of the new virtual machine"
     value = module.virtual-machine.vm_id
 }
+
+output "customdata" {
+  value = data.local_file.cloudinit.content
+}
+
+output "vm_public_key" {
+    description = "This is the vm public key"
+    value = module.virtual-machine.ssh_public_key    
+}
+
+output "ssh_private_key" {
+    description = "This is the vm private key"
+    value = tls_private_key.ssh_key.id    
+}
